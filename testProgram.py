96702b90230a15ec8e83a0cb83e3511202e04fb2
Kabaczek666 <57800470+Kabaczek666@users.noreply.github.com>
2019-11-15 20:18:17 +0100

    Update testProgram.py

--- testProgram.py
+++ testProgram.py
@@ -1,11 +1,10 @@
-from node import Node
-from rack import Rack
-from regneklynge import Regneklynge
+from datasenterVP import Datasenter
 
-n1 = Node(32, 2)
-n2 = Node(4, 1)
-n3 = Node(8, 2)
+data = Datasenter()
 
-r = Regneklynge(10)
+filer = ["abel.txt", "saga.txt"]
 
-r.test(33, 2, 2)
+for fil in filer:
+    data.lesInnRegneklynge(fil)
+
+data.skrivUtAlleRegneklynger()
